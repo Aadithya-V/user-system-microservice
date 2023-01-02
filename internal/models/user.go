d2caae106c5b8b1cb0108bd853e269f0737abb2d
Aadithya V <70337236+Aadithya-V@users.noreply.github.com>
2023-01-03 00:11:16 +0530

    fix file: ensure min registration details provided
    Required binding added.

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,13 +3,13 @@ package models
 // struct user represents data of user.
 type User struct {
 	ID          string  `json:"id"` //vet bindings later- binding:"required"
-	Name        string  `json:"name"`
+	Name        string  `json:"name" binding:"required"`
 	DOB         string  `json:"dob"`
-	Address     string  `json:"address" binding:"required"`
+	Address     string  `json:"address"`
 	Latitude    float64 `json:"latitude" binding:"required"`
 	Longitude   float64 `json:"longitude" binding:"required"`
 	Description string  `json:"description"`
 	CreatedAt   string  `json:"createdAt"`
-	Pwd         string  `json:"pwd"`
+	Pwd         string  `json:"pwd" binding:"required"`
 	Auth        string  `json:"auth"`
 }
